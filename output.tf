f1949cbcdc2be6a597c7434a5e10efe6f3e0adf9
Craig Thacker <craigthackerx@gmail.com>
2022-11-06 21:17:29 +0000

    Update module

--- output.tf
+++ output.tf
@@ -1,7 +1,19 @@
 output "nsg_id" {
-  value = azurerm_network_security_group.nsg.id
+  value       = azurerm_network_security_group.nsg.id
+  description = "The ID of the NSG"
 }
 
 output "nsg_name" {
-  value = azurerm_network_security_group.nsg.name
+  value       = azurerm_network_security_group.nsg.name
+  description = "The name of the NSG"
+}
+
+output "nsg_rg_name" {
+  value       = azurerm_network_security_group.nsg.resource_group_name
+  description = "The name of the resource group the nsg is in"
+}
+
+output "nsg_rules" {
+  value       = var.standard_nsg_list
+  description = "The NSG rules list assigned as a variable"
 }
